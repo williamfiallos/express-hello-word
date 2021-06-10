97406590675c118fbb3dd01977a1332c7ea46cc4
William Fiallos <wfiallos10@gmail.com>
2021-06-10 18:10:23 -0400

    update express app

--- app.js
+++ app.js
@@ -3,23 +3,29 @@
 const express = require('express'); //'express' is name of packaged installed
 
 // We create our own server named app
-// Express server handling requests and responses
+// Express server will be handling requests and responses
 const app = express();
 
+// Middleware
+// Make everything inside of public/ available
 app.use(express.static('public'));
 
-
-// '/' means localhost:3000
-// callback function as 2nd argument
-app.get('/', (request, response, next) => {
-  console.log("Something here will be displayed in the browser.");
-  // response.send('<p>Hello there, <b>Ironhackers</b>!!!</p>');
-  response.sendFile(__dirname + '/views/home.html');
+// our first Route:
+// callback function as 2nd argument. This is the function that will be called when 
+// someone makes a request to /home (in this example) or to /some-route in some other case
+app.get('/home', (request, response, next) => {
+  // console.log(request);
+  response.sendFile(__dirname + '/views/home-page.html');
 });
+// Or
+// app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
 
-app.get('/cat', (req, res, next) => {
-  // res.send('src="/images/cool-cat.jpg');
-  res.sendFile(__dirname + '/views/cat.html');
+// cat route:
+app.get('/cat', (request, response, next) => {
+  // console.log(__dirname);
+  response.sendFile(__dirname + '/views/cat-page.html');
 });
+// Or
+// app.get('/cat', (request, response, next) => response.sendFile(__dirname + '/views/cat-page.html'));
 
-app.listen(3000, () => console.log("My first express app running on port 3000."));
+app.listen(8080, () => console.log("My first express app running on port 8080."));
